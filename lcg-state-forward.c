9b7b49204fafcdaacee71420f5df6928cd93b446
Jacques Uber <uberj@miradorfin.com>
2017-05-20 12:12:28 -0700

    [master] wip

--- lcg-state-forward.c
+++ lcg-state-forward.c
@@ -14,6 +14,21 @@
 #define MODMULT(a, b, c, m, s) q = s/a;s=b*(s-a*q)-c*q;if(s<0)s+=m
 /* MODMULT computes s*b mod m, provided that m=a*b+c and 0<=c<m */
 
+void modMult_(int *s1, int *s2) {
+	int q;
+        MODMULT(53668, 40014, 12211, 2147483563L, *s1);
+        MODMULT(52774, 40692, 3791, 2147483399L, *s2);
+}
+
+void lcg_value(double *z, int *s1, int *s2) {
+        modMult_(s1, s2);
+
+        *z = *s1 - *s2;
+        if (*z < 1)
+                *z += 2147483562;
+        *z *= 4.656613e-10;
+}
+
 int main(int argc, char** argv)
 {
 	if (argc != 4)
@@ -32,33 +47,9 @@ int main(int argc, char** argv)
 
 	while (times++ != total)
 	{
-	    MODMULT(53668, 40014, 12211, 2147483563L, s1);
-	    MODMULT(52774, 40692, 3791, 2147483399L, s2);
-
-		z = s1 - s2;
-		if (z < 1)
-			z += 2147483562;
-		z *= 4.656613e-10;
-
+                lcg_value(&z, &s1, &s2);
 		printf("%d: s1=%d s2=%d lcg_value=%.14f\n", times, s1, s2, z);
 	}
 
 	return 0;
 }
-
-double lch_value(int *s1_ref, int *s2_ref) {
-        double z;
-        MODMULT(53668, 40014, 12211, 2147483563L, *s1_ref);
-        MODMULT(52774, 40692, 3791, 2147483399L, *s2_ref);
-
-        z = *s1_ref - *s2_ref;
-        if (z < 1)
-                z += 2147483562;
-        z *= 4.656613e-10;
-        return z;
-}
-
-/*
- * for 
- */
-
